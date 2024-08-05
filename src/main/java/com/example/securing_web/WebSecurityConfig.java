ab669c2a76828728be3af2f35cee8de2cfc60e63
Gareth NORMAN <norman.gareth@protonmail.com>
2024-08-05 13:44:55 +0800

    Initial commit

---
+++ src/main/java/com/example/securing_web/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.example.securing_web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author gareth
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/", "/home").permitAll().anyRequest().authenticated())
+				.formLogin((form) -> form.loginPage("/login").permitAll()).logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
